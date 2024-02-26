048af2458af13080c7de813fccc255af8ff16d88
TheDummyUser <abhiram.reddy122002@gmail.com>
2024-02-26 15:35:30 +0530

    small change

--- src/components/Heder.jsx
+++ src/components/Heder.jsx
@@ -10,27 +10,27 @@ const Heder = () => {
       border-red-300
     "
     >
-      <div className="flex justify-end space-x-6">
+      <div className="flex justify-end md:space-x-6 space-x-3">
         <p className="">
           {" "}
-          <button className="inline-flex md:h-10 h-6 md:font-medium md:font-JetBrains font-JetBrains text-sm animate-shimmer items-center justify-center rounded-md border border-slate-800 bg-[linear-gradient(110deg,#000103,45%,#1e2631,55%,#000103)] bg-[length:200%_100%] px-6 text-slate-400 transition-colors focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50">
+          <button className="inline-flex md:h-10 h-8 md:font-medium md:font-JetBrains font-JetBrains text-sm animate-shimmer items-center justify-center rounded-md border border-slate-800 bg-[linear-gradient(110deg,#000103,45%,#1e2631,55%,#000103)] bg-[length:200%_100%] px-3 text-slate-400 transition-colors focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50">
             <Link to="/">Home</Link>
           </button>
         </p>
         <p>
           {" "}
-          <button className="inline-flex md:h-10 h-6 md:font-medium md:font-JetBrains font-JetBrains text-sm animate-shimmer items-center justify-center rounded-md border border-slate-800 bg-[linear-gradient(110deg,#000103,45%,#1e2631,55%,#000103)] bg-[length:200%_100%] px-6 text-slate-400 transition-colors focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50">
+          <button className="inline-flex md:h-10 h-8 md:font-medium md:font-JetBrains font-JetBrains text-sm animate-shimmer items-center justify-center rounded-md border border-slate-800 bg-[linear-gradient(110deg,#000103,45%,#1e2631,55%,#000103)] bg-[length:200%_100%] px-3 text-slate-400 transition-colors focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50">
             <Link to="/about"> About</Link>
           </button>
         </p>
         <p>
           {" "}
-          <button className="inline-flex md:h-10 h-6 md:font-medium md:font-JetBrains font-JetBrains text-sm animate-shimmer items-center justify-center rounded-md border border-slate-800 bg-[linear-gradient(110deg,#000103,45%,#1e2631,55%,#000103)] bg-[length:200%_100%] px-6 text-slate-400 transition-colors focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50">
+          <button className="inline-flex md:h-10 h-8 md:font-medium md:font-JetBrains font-JetBrains text-sm animate-shimmer items-center justify-center rounded-md border border-slate-800 bg-[linear-gradient(110deg,#000103,45%,#1e2631,55%,#000103)] bg-[length:200%_100%] px-3 text-slate-400 transition-colors focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50">
             <Link to="/projects"> Projects</Link>
           </button>
         </p>
         <p>
-          <button className="inline-flex md:h-10 h-6 md:font-medium md:font-JetBrains font-JetBrains text-sm animate-shimmer items-center justify-center rounded-md border border-slate-800 bg-[linear-gradient(110deg,#000103,45%,#1e2631,55%,#000103)] bg-[length:200%_100%] px-6 text-slate-400 transition-colors focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50">
+          <button className="inline-flex md:h-10 h-8 md:font-medium md:font-JetBrains font-JetBrains text-sm animate-shimmer items-center justify-center rounded-md border border-slate-800 bg-[linear-gradient(110deg,#000103,45%,#1e2631,55%,#000103)] bg-[length:200%_100%] px-3 text-slate-400 transition-colors focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50">
             <Link to="/contactme">Contact Me</Link>
           </button>
         </p>
